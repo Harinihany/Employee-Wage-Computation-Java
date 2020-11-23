37534380f99cd8bacea7c1df0596dcda1d88ab21
Harinihany <harinihany03@gmail.com>
2020-11-23 19:59:10 +0530

    added :switch case to check partTime/fullTime

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -26,40 +26,21 @@ public class EmpWageComputation  {
 
 		System.out.println("Random number: "+randomNum);
 
-		
-
-		 //int workHour=8;
-                 //int salary=(empRatePerHour * workHour);
-
-		if(randomNum==isFullTime)
-
-		{
-
-			System.out.println("Employee is Full-Time");
-			int workHour=8;
-			int salary=(empRatePerHour * workHour);
-
-			System.out.println("salary is:"+salary);
-		}
-		else if(randomNum==isPartTime)
-		//int workHour=4;
-
-                {
-
-                        System.out.println("Employee is Part-Time");
-			int workHour=4;
-			int salary=(empRatePerHour * workHour);
-
-                        System.out.println("salary is:"+salary);
-                }
-
-		else
-
-		{
-
-			System.out.println("Employee is Absent");
 	
-			System.out.println("salary is:"+0);
+
+		
+		switch (randomNum)
+		{
+
+		case 1: 
+		
+			System.out.println("Employee is Full-Time");
+			break;
+		case 2: System.out.println("Employee is Part-Time");
+                	break;
+
+		default: System.out.println("Employee is Absent");
+
 		}
 
 
