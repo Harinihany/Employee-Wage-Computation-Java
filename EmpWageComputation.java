1c771c66cb7f849e749cc756929e828f6b0d5134
Harinihany <harinihany03@gmail.com>
2020-11-23 21:52:24 +0530

    added: logic to calculate one month salary

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -17,7 +17,10 @@ public class EmpWageComputation  {
 		int empRatePerHour=20;
 		int isFullTime=1;
 		int isPartTime=2;
-
+		int numOfWorkingDays=20;
+		int empHrs=0;
+                int salary=0;
+		int totalSalary=0;
 
 		int randomNum=random.nextInt(3);
 
@@ -25,30 +28,28 @@ public class EmpWageComputation  {
 		
 
 		System.out.println("Random number: "+randomNum);
-
-	
-
-		
+		for (int day=1; day<=20; day++)	{	
 		switch (randomNum)
 		{
 
 		case 1: 
 		
 			System.out.println("Employee is Full-Time");
+			empHrs=8;
 			break;
 		case 2: System.out.println("Employee is Part-Time");
-                	break;
-
-		default: System.out.println("Employee is Absent");
-
+			empHrs=4;
+			break;
+                default: System.out.println("Employee is Absent");
+			empHrs=0;
 		}
 
+		perDaySalary=empRatePerHour*empHrs;
+                System.out.println("Salary for the day is "+perDaySalary);
+		totalSalary1=totalSalary+perDaySalary
+		}
 
+		 System.out.println("Salary for the month is "+totalSalary1);
 
-
-	}
-
-
-
-
+		
 }
