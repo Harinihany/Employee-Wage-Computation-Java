b030cffcd6bda16fb5f9ad339e4074e4188b89b6
Harinihany <harinihany03@gmail.com>
2020-11-23 17:39:28 +0530

    added: code for salary calculation

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,7 +1,58 @@
 
 
-class EmpWageComputation {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program"); 
-    }
+
+import java.util.Random;
+
+public class EmpWageComputation  {
+
+
+
+
+	public static void main(String[] args) {
+
+		
+
+		Random random=new Random();
+		//int salary=0;
+		int empRatePerHour=20;
+		
+
+		int randomNum=random.nextInt(2);
+
+		System.out.println("Welcome to Employee Wage Computation Program");
+		
+
+		System.out.println("Random number: "+randomNum);
+
+		
+
+		 int workHour=8;
+                 int salary=(empRatePerHour * workHour);
+
+		if(randomNum==1)
+
+		{
+
+			System.out.println("Employee is Present");
+			
+			System.out.println("salary is:"+salary);
+		}
+
+		else
+
+		{
+
+			System.out.println("Employee is Absent");
+	
+			System.out.println("salary is:"+0);
+		}
+
+
+
+
+	}
+
+
+
+
 }
