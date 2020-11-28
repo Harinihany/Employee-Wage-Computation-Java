62b2295851ce677fa54faba2302d9e54e0fbde5b
Harinihany <harinihany03@gmail.com>
2020-11-28 16:57:35 +0530

    added: code for total age; when queried by company name

--- IComputeEmpWage.java
+++ IComputeEmpWage.java
@@ -4,6 +4,7 @@ public interface IComputeEmpWage {
 	 
 	 public void addCompanyEmpWage(String company,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth);
 	 public void computeEmpWage();
-	 public int computeEmpWage(CompanyEmpWage companyEmpWage);
-	  
+	 
+	 public int getTotalWage(String company);
+ 
 }
