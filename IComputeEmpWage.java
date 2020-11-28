de5cabb7e7453723a07450473ef91c74712ac9d5
Harinihany <harinihany03@gmail.com>
2020-11-28 15:55:02 +0530

    added: interface logic to manage emp-wage of multiple companies

---
+++ IComputeEmpWage.java
@@ -0,0 +1,9 @@
+
+
+public interface IComputeEmpWage {
+	 
+	 public void addCompanyEmpWage(String company,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth);
+	 public void computeEmpWage();
+	 public int computeEmpWage(CompanyEmpWage companyEmpWage);
+	  
+}
